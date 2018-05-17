55367931b3bc94563e67bf25b4c471947c3e4e92
Pedro Perri <pedropgp@al.insper.edu.br>
2018-05-17 16:20:17 -0300

    Inserção de tiros

--- P Final.py
+++ P Final.py
@@ -7,10 +7,6 @@ Created on Tue May  8 09:36:34 2018
 
 
 import pygame
-from os import path
-from random import randrange
-import random
-
 
 #======================= CLASSES ===========================
 class Personagem (pygame.sprite.Sprite):
@@ -21,32 +17,32 @@ class Personagem (pygame.sprite.Sprite):
         self.image.set_colorkey(white)
         self.rect = self.image.get_rect()
         self.rect.x = 150
-        self.rect.y = 150
+        self.rect.y = 550
         self.vx = 0
         
-    def move (self):
+    def update(self):
         self.vx = 0 
         self.vy = 0
         keystate = pygame.key.get_pressed()
         
         #### MOVIMENTO DA NAVE ####
-        # x^2 + x^2 = 25 ---- x^2 =12.5 ---- x = 12.5 ** (1/2)
+        
         if keystate[pygame.K_LEFT] and not keystate[pygame.K_RIGHT] and not\
         keystate[pygame.K_UP] and not keystate[pygame.K_DOWN]:
-            self.vx = -(12.5 ** (1/2))
-            self.vy = -(12.5 ** (1/2))
+            self.vx = -6
+            self.vy = -3.5
         if keystate[pygame.K_RIGHT] and not keystate[pygame.K_LEFT] and not\
         keystate[pygame.K_UP] and not keystate[pygame.K_DOWN]:
-            self.vx = (12.5 ** (1/2))
-            self.vy = (12.5 ** (1/2))
+            self.vx = 4
+            self.vy = 3.5
         if keystate[pygame.K_UP] and not keystate[pygame.K_DOWN] and not\
         keystate[pygame.K_LEFT] and not keystate[pygame.K_RIGHT]:
-            self.vx = (12.5 ** (1/2))
-            self.vy = -(12.5 ** (1/2))
+            self.vx = 6
+            self.vy = -3.5
         if keystate[pygame.K_DOWN] and not keystate[pygame.K_UP] and not\
         keystate[pygame.K_LEFT] and not keystate[pygame.K_RIGHT]:
-            self.vx = -(12.5 ** (1/2))
-            self.vy = (12.5 ** (1/2))
+            self.vx = -4
+            self.vy = 3.5
         
         #### OUTROS MOVIMENTOS ####
         if keystate[pygame.K_LEFT] and keystate[pygame.K_DOWN]:
@@ -69,30 +65,31 @@ class Personagem (pygame.sprite.Sprite):
             self.rect.top = 5
         
         
-    def tiro(self, tudo, tiro_group):
-        tiro = Tiros('imagens/tiro_azul.png', self.rect.x, self.rect.top, self.rect.right)
+    def Tiro(self, tudo, tiros_group):
+        tiro = Tiros('imagens/tiro_azul.png', self.rect.centerx, self.rect.y)
         tudo.add(tiro)
-        tiro_group.add(tiro)
+        tiros_group.add(tiro)
         
 
 class Tiros(pygame.sprite.Sprite):
     
-    def __init__(self, arquivo_imagem):
+    def __init__(self, arquivo_imagem, pos_y, pos_x):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(arquivo_imagem)
         self.rect = self.image.get_rect()
-        self.rect.y = 150
-        self.rect.x = 150
-        self.vy = -(12.5 ** (1/2))
-        self.vx = -(12.5 ** (1/2))
+        self.rect.bottom = pos_y
+        self.rect.centerx = pos_x
+        self.vy = -12.5 ** (1/2)
+        self.vx = 12.5 ** (1/2)
         
-    def move(self):
-        self.rect.y = self.vy
-        if self.rect.y < 0:
-            self.kill
-        if self.rect.x < 0:
-            self.kill
-#===================== Cores ============================= 
+    def update(self):
+        self.rect.y += self.vy
+        self.rect.x += self.vx
+        if self.rect.bottom < 0:
+            self.kill()
+        if self.rect.right < 0:
+            self.kill()
+#===================== CORES ============================= 
        
 black = (0,0,0)
 white = (255,255,255)
@@ -100,74 +97,93 @@ gray = (128, 128, 128)
 
 red = (200,0,0)
 green = (0,200,0)
+blue = (0, 0, 200)
+yellow = (200, 200, 0)
+purple = (200, 0, 200)
 
 bright_red = (255,0,0)
 bright_green = (0,255,0)
+bright_blue = (0, 0, 255)
+bright_yellow = (255, 255, 0)
+bright_purple = (255, 0, 255)
+#=============== CONFIGURAÇÕES DA TELA ===========
+largura_tela = 1000
+altura_tela = 700     
+
+#================ BOTÕES ===============
+"BOTÃO VERDE"
+x_green = largura_tela/2 - 170
+y_green = altura_tela/2 
+w_green = 350
+h_green = 50
+    
+"BOTÃO VERMELHO"
+x_red = largura_tela/2 - 170
+y_red = altura_tela/2 + 100
+w_red = 350
+h_red = 50
         
+#CARACTERÍSTICAS DO BOTÃO AZUL (X, Y, W, H)
+#x_blue = largura_tela/2 - 170
+#y_blue = altura_tela/2 + 200
+#w_blue = 350
+#h_blue = 50      
+
 
 #===================== FUNÇÕES =============================
         
 def sair ():
-    pygame.QUIT()
+    pygame.quit()
     quit()
         
 def loop ():
 
-    x = 0
     y = 0
     Game = True
 
-    tela.blit(fundo, (0,0))
-    
     while Game:
         
         relogio.tick(100)
-        
+    
         pressed_keys = pygame.key.get_pressed()
         
         if pressed_keys[pygame.K_ESCAPE]:
             Game = False
-        
+            sair()
+    
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()       
+            if event.type == pygame.QUIT:            
+                Game = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    personagem.tiro(tudo, tiro_group)
-              
+                    personagem.Tiro(tudo, tiros_group)
+
         ##### MOVIMENTO DA TELA  #####
         rel_y = y % fundo.get_rect().height
         tela.blit (fundo, (0, rel_y - fundo.get_rect().height))
-        if rel_y < altura_tela:
-            tela.blit (fundo, (0, rel_y))
-        y += 12.5 ** (1/2)
-        
-        rel_x = x % fundo.get_rect().width
-        tela.blit (fundo, (0, rel_x - fundo.get_rect().width))
-        if rel_x < largura_tela:
-            tela.blit (fundo, (0, rel_x))
-        x += 12.5 ** (1/2)
-            
+        if rel_y < altura_tela :
+            tela.blit (fundo, (rel_y, 0))
+        y += 7
+      
+      
         tudo.update()
-            
-        personagem.move()
         
         tudo.draw(tela)
         pygame.display.flip()
        
+
 #https://pythonprogramming.net/adding-sounds-music-pygame/  --> Menu
         
 def text_objects(texto, fonte):
-    
+    pygame.font.get_fonts()
     textSurface = fonte.render(texto, True, white)
     return textSurface, textSurface.get_rect()
 
 def botoes(msg, x, y, w, h, ic, ac, action = None):
     
     #colocando os botões
-    # x: The x location of the top left coordinate of the button box.
-    # y: The y location of the top left coordinate of the button box.
+    # x: A posição no eixo x da caixa do botão.
+    # y: A posição no eixo y da caixa do botão.
     # w: Button width.
     # h: Button height.
     # ic: Inactive color (when a mouse is not hovering).
@@ -183,48 +199,39 @@ def botoes(msg, x, y, w, h, ic, ac, action = None):
     else:
         pygame.draw.rect(tela, ic, (x, y, w, h))
         
-    smallText = pygame.font.SysFont("ARBONNIE", 35)
+    smallText = pygame.font.SysFont("rockwellcondensed", 45)
     textSurf, textRect = text_objects(msg, smallText)
     textRect.center = ((x + (w / 2)), (y + (h / 2)))
     tela.blit(textSurf, textRect)
         
-def start_menu():
-
-    start = True
-
-    while start:
-        
-        #Características do o botão verde (x, y, w, h)
-        x_green = largura_tela/2 - 170
-        y_green = altura_tela/2 + 100
-        w_green = 350
-        h_green = 50
     
-        #Características do  botão vermelho(x, y, w, h)
-        x_red = largura_tela/2 - 170
-        y_red = altura_tela/2 + 200
-        w_red = 350
-        h_red = 50
-        
-        fundo = pygame.image.load("imagens/fundo.png").convert()
-        
+def menu():
+    x = 0
+    start = True
+    fnd = pygame.image.load("imagens/fundo.png").convert()
+    while start:
         for event in pygame.event.get():
-            #print(event)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
                 
-            tela.blit(fundo, (0, 0))
-            largeText = pygame.font.SysFont("Goudystout", 70)
-            TextSurf, TextRect = text_objects("Space Run", largeText)
-            TextRect.center = ((largura_tela / 2), (altura_tela / 3))
-            tela.blit(TextSurf, TextRect)
+        rel_x = x % fnd.get_rect().width
+        tela.blit(fnd, (rel_x -fnd.get_rect().width, 0))
+        if rel_x < largura_tela:
+            tela.blit (fnd, (rel_x, 0))
+        x += 2
+        
+        
+        largeText = pygame.font.SysFont("rockwellcondensed", 120)
+        TextSurf, TextRect = text_objects("Space Run", largeText)
+        TextRect.center = ((largura_tela / 2), (altura_tela / 3))
+        tela.blit(TextSurf, TextRect)
 
-            botoes("Start", x_green, y_green, w_green, h_green, green, bright_green, loop)
-            botoes("Sair", x_red, y_red, w_red, h_red, red, bright_red, sair)
+        botoes("Start", x_green, y_green, w_green, h_green, green, bright_green, loop)
+        botoes("Quit", x_red, y_red, w_red, h_red, red, bright_red, sair)
 
-            pygame.display.update()
-            relogio.tick(15)
+        pygame.display.update()
+        relogio.tick(15)
         
         
 #===================== INÍCIO ======================
@@ -249,11 +256,8 @@ personagem =  Personagem("imagens/personagem.png")
 personagem_group.add(personagem)
 tudo.add(personagem)
 
-tiro_group = pygame.sprite.Group()
-tiro = Tiros('imagens/tiro_azul.png')
-tiro_group.add(tiro)
-tudo.add(tiro)
+tiros_group = pygame.sprite.Group()
 
 
-start_menu()
+menu()
 loop()
