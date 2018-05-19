e89917e64c237a13056c59df26268a6398ed96c2
arthurpf1 <38359269+arthurpf1@users.noreply.github.com>
2018-05-19 17:57:14 -0300

    sons/pontuaçao
    .

--- P Final.py
+++ P Final.py
@@ -9,6 +9,14 @@ Created on Tue May  8 09:36:34 2018
 import pygame
 
 #======================= CLASSES ===========================
+
+def score(score):
+    largeText = pygame.font.SysFont("None", 50)
+    banana = largeText.render("score: "+str(score),0,(255,255,255))
+    tela.blit(banana,(0,0))
+    pygame.display.update()
+
+
 class Personagem (pygame.sprite.Sprite):
     
     def __init__(self, arquivo_imagem):
@@ -18,55 +26,69 @@ class Personagem (pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 150
         self.rect.y = 550
-        self.vx = 0
-        
+        self.vx = 0        
+
     def update(self):
         self.vx = 0 
         self.vy = 0
         keystate = pygame.key.get_pressed()
         
-        #### MOVIMENTO DA NAVE ####
-        
+#### MOVIMENTO DA NAVE ####
+
+#### Nave movendo para a diagonal esquerda alta ####
         if keystate[pygame.K_LEFT] and not keystate[pygame.K_RIGHT] and not\
         keystate[pygame.K_UP] and not keystate[pygame.K_DOWN]:
             self.vx = -6
             self.vy = -3.5
+#### Nave movendo para a diagonal direita baixa ####
         if keystate[pygame.K_RIGHT] and not keystate[pygame.K_LEFT] and not\
         keystate[pygame.K_UP] and not keystate[pygame.K_DOWN]:
             self.vx = 4
             self.vy = 3.5
+#### Nave movendo para a diagonal direita alta ####
         if keystate[pygame.K_UP] and not keystate[pygame.K_DOWN] and not\
         keystate[pygame.K_LEFT] and not keystate[pygame.K_RIGHT]:
             self.vx = 6
             self.vy = -3.5
+#### Nave movendo para a diagonal esquerda baixa ####
         if keystate[pygame.K_DOWN] and not keystate[pygame.K_UP] and not\
         keystate[pygame.K_LEFT] and not keystate[pygame.K_RIGHT]:
             self.vx = -4
             self.vy = 3.5
         
-        #### OUTROS MOVIMENTOS ####
+#### OUTROS MOVIMENTOS ####
+        
+#### Nave movendo para a esquerda ####
         if keystate[pygame.K_LEFT] and keystate[pygame.K_DOWN]:
             self.vx = -5
+#### Nave movendo para cima ####
         if keystate[pygame.K_LEFT] and keystate[pygame.K_UP]:
             self.vy = -5
+#### Nave movendo para baixo ####
         if keystate[pygame.K_RIGHT] and keystate[pygame.K_DOWN]:
             self.vy = 5
+#### Nave movendo para a direita ####
         if keystate[pygame.K_RIGHT] and keystate[pygame.K_UP]:
             self.vx = 5
         self.rect.x += self.vx
         self.rect.y += self.vy        
+       
+#### LIMITES DA NAVE NA TELA ####
+#### Lado direito da tela ####        
         if self.rect.right > largura_tela -5:
             self.rect.right = largura_tela - 5
+#### Lado esquerdo da tela ####
         if self.rect.left < 5:
             self.rect.left = 5
+#### Parte de baixo da tela ####
         if self.rect.bottom > altura_tela - 5:
             self.rect.bottom = altura_tela - 5
+#### Parte de cima da tela ####
         if self.rect.top < 5:
             self.rect.top = 5
-        
-        
+            
     def Tiro(self, tudo, tiros_group):
-        tiro = Tiros('imagens/tiro_azul.png', self.rect.centerx, self.rect.y)
+        tiro = Tiros('imagens/tiro_azul.png', self.rect.top - 15, self.rect.centerx + 50)
         tudo.add(tiro)
         tiros_group.add(tiro)
         
@@ -77,20 +99,22 @@ class Tiros(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(arquivo_imagem)
         self.rect = self.image.get_rect()
-        self.rect.bottom = pos_y
+        self.rect.top = pos_y
         self.rect.centerx = pos_x
-        self.vy = -12.5 ** (1/2)
-        self.vx = 12.5 ** (1/2)
+        self.vy = -3.5
+        self.vx = 6
         
     def update(self):
         self.rect.y += self.vy
         self.rect.x += self.vx
+#### Tiro morre quando sai da tela (kill) #### 
         if self.rect.bottom < 0:
             self.kill()
         if self.rect.right < 0:
             self.kill()
 #===================== CORES ============================= 
-       
+
+#### Combinação de numeros caracteriza as cores RGB ####
 black = (0,0,0)
 white = (255,255,255)
 gray = (128, 128, 128)
@@ -106,11 +130,13 @@ bright_green = (0,255,0)
 bright_blue = (0, 0, 255)
 bright_yellow = (255, 255, 0)
 bright_purple = (255, 0, 255)
+
+
 #=============== CONFIGURAÇÕES DA TELA ===========
 largura_tela = 1000
 altura_tela = 700     
-
 #================ BOTÕES ===============
+
 "BOTÃO VERDE"
 x_green = largura_tela/2 - 170
 y_green = altura_tela/2 
@@ -129,55 +155,74 @@ h_red = 50
 #w_blue = 350
 #h_blue = 50      
 
+#===================== FUNÇÕES =============================     
+    
 
-#===================== FUNÇÕES =============================
-        
+#### Função para sair da tela de jogo ####        
 def sair ():
     pygame.quit()
     quit()
-        
+    
+#### Função loop que faz o jogo rodar ####
 def loop ():
-
+    x = 0
     y = 0
     Game = True
+    pygame.mixer.music.play(-1)
 
     while Game:
         
         relogio.tick(100)
     
         pressed_keys = pygame.key.get_pressed()
-        
+#### ESC sai do jogo ####        
         if pressed_keys[pygame.K_ESCAPE]:
             Game = False
             sair()
     
         for event in pygame.event.get():
             if event.type == pygame.QUIT:            
+                pygame.mixer.music.stop()
                 Game = False
+#### SPACE atira ####                
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     personagem.Tiro(tudo, tiros_group)
+                    pygame.mixer.Sound.play(som_tiro)
 
-        ##### MOVIMENTO DA TELA  #####
+#### MOVIMENTO DA TELA ####      
+      
+        rel_x = x % fundo.get_rect().width
         rel_y = y % fundo.get_rect().height
-        tela.blit (fundo, (0, rel_y - fundo.get_rect().height))
-        if rel_y < altura_tela :
-            tela.blit (fundo, (rel_y, 0))
-        y += 7
-      
-      
-        tudo.update()
+        tela.blit(fundo, (rel_x - fundo.get_rect().width, rel_y - fundo.get_rect().height))
+        if rel_x < largura_tela or rel_y < altura_tela:
+            tela.blit (fundo, (rel_x, rel_y))
+        x -= 4
+        y += 4
         
+        if rel_x == 300 and rel_y == 200:
+            rel_x = 0
+            rel_y = 0
+        tela.blit (fundo, (rel_x, rel_y))
+ 
+        #####PONTUACAO#####
+        score(int(pygame.time.get_ticks()/1000))
+        
+        tudo.update()
+#Desenhar tudo que está no grupo "tudo" na tela ####        
         tudo.draw(tela)
         pygame.display.flip()
        
 
+#### REFERÊNCIA PRO MENU ####
 #https://pythonprogramming.net/adding-sounds-music-pygame/  --> Menu
-        
+
+#### Função das letras nos botões ####
 def text_objects(texto, fonte):
     pygame.font.get_fonts()
     textSurface = fonte.render(texto, True, white)
     return textSurface, textSurface.get_rect()
+
 
 def botoes(msg, x, y, w, h, ic, ac, action = None):
     
@@ -189,8 +234,10 @@ def botoes(msg, x, y, w, h, ic, ac, action = None):
     # ic: Inactive color (when a mouse is not hovering).
     # ac: Active color (when a mouse is hovering).
     
+    
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
+    
 
     if x + w > mouse[0] > x and y + h > mouse[1] > y:
         pygame.draw.rect(tela, ac, (x, y, w, h))
@@ -198,6 +245,7 @@ def botoes(msg, x, y, w, h, ic, ac, action = None):
             action()         
     else:
         pygame.draw.rect(tela, ic, (x, y, w, h))
+        
         
     smallText = pygame.font.SysFont("rockwellcondensed", 45)
     textSurf, textRect = text_objects(msg, smallText)
@@ -219,7 +267,7 @@ def menu():
         tela.blit(fnd, (rel_x -fnd.get_rect().width, 0))
         if rel_x < largura_tela:
             tela.blit (fnd, (rel_x, 0))
-        x += 2
+        x -= 4
         
         
         largeText = pygame.font.SysFont("rockwellcondensed", 120)
@@ -235,12 +283,17 @@ def menu():
         
         
 #===================== INÍCIO ======================
+        
+        
 pygame.init()
+
+pygame.mixer.music.load("sons/musica.mp3")
+som_tiro=pygame.mixer.Sound("sons/tiro.ogg")
 
 largura_tela = 1000
 altura_tela = 700
 
-tela=pygame.display.set_mode((largura_tela, altura_tela), 0, 32)
+tela = pygame.display.set_mode((largura_tela, altura_tela), 0, 32)
 
 pygame.display.set_caption('STARSHIPS')
 
@@ -248,7 +301,10 @@ relogio = pygame.time.Clock()
     
 fundo = pygame.image.load("imagens/fundo.png").convert()
 
+
 #=================  CRIANDO GRUPOS  ========================
+
+
 tudo = pygame.sprite.Group()
 
 personagem_group = pygame.sprite.Group()
